dbc8c1fea16a704a2420a9ffceafd489e75f7cc8
youcancallmejay <148828634+youcancallmejay@users.noreply.github.com>
2024-07-23 16:26:46 -0500

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,32 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext> (opt => 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+});
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+// Configure CORS to allow requests from your React app
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .WithOrigins("http://localhost:3000"); // Allow your React app's URL
+    });
+});
 
 var app = builder.Build();
 
@@ -21,29 +34,28 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-using var scope = app.Services.CreateScope(); 
+using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
 try
 {
-    var context = services.GetRequiredService<DataContext>(); 
-    await context.Database.MigrateAsync(); 
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
     await Seed.SeedData(context);
 }
 catch (Exception ex)
 {
-    
-    var logger = services.GetRequiredService<ILogger<Program>>(); 
-    logger.LogError(ex, "An error occured during mirgation.");
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration.");
 }
-
-
 
 app.Run();
